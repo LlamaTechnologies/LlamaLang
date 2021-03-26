bd8791d5f5364f1a5dc4616b2e2aeec136b3dcf5
Pablo Narvaja <pnarvaja.21@gmail.com>
2021-03-25 21:35:33 -0300

    AST node

--- src/ast/FunctionCallNode.hpp
+++ src/ast/FunctionCallNode.hpp
@@ -7,6 +7,9 @@ namespace llang::ast
      * Represents a call to a function
     */
     struct FunctionCallNode : public StatementNode {
+        std::string Name;
+        std::vector<std::shared_ptr<StatementNode>> Parameters;
+        std::shared_ptr<FunctionDefNode> functionFound;
 
         FunctionCallNode() :
             StatementNode(STATEMENT_TYPE::CALL) {}
@@ -16,7 +19,18 @@ namespace llang::ast
         }
 
         void ToString(std::string &str, const int tabLevel) const override {
+            auto tabs = GetTabs(tabLevel);
+            str += tabs + Name + "(";
 
+            // print parameters
+            size_t i = 0;
+            for (auto param : Parameters) {
+                param->ToString(str, 0);
+                if (++i != Parameters.size())
+                    str += ", ";
+            }
+
+            str += ")";
         }
     };
 }
