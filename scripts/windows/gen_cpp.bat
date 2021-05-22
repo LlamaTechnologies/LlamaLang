d29724b27da40ad5a3990308c54eeca9431cc6a1
Pablo Narvaja <pnarvaja.21@gmail.com>
2021-05-22 20:48:40 -0300

    deleted unused scripts

--- scripts/windows/gen_cpp.bat
+++
@@ -1,2 +0,0 @@
-java -jar ../antlr-4.9.1-complete.jar -listener -no-visitor -Dlanguage=Cpp -package "llang" ../../ModuleRetriever.g4 -o ../../src/antlr
-java -jar ../antlr-4.9.1-complete.jar -no-listener -visitor -Dlanguage=Cpp -package "llang" ../../LlamaLang.g4 -o ../../src/antlr
--- scripts/windows/gen_moduleretriever_cpp.bat
+++
@@ -1,1 +0,0 @@
-java -jar ../antlr-4.9.1-complete.jar -listener -no-visitor -Dlanguage=Cpp -package "llang" ../../ModuleRetriever.g4 -o ../../src/antlr
