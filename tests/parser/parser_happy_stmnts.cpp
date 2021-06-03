c9ffb298620ade51950aab19b8a0a3cc3c3b78b5
Pablo Narvaja <pnarvaja.21@gmail.com>
2021-06-03 18:53:30 -0300

    Update parser_happy_stmnts.cpp

--- tests/parser/parser_happy_stmnts.cpp
+++ tests/parser/parser_happy_stmnts.cpp
@@ -54,4 +54,55 @@ TEST(ParserHappyStmntTests, AssignStmntTest) {
     ASSERT_EQ(value_node->binary_expr.op2->node_type, AstNodeType::AstBinaryExpr);
 }
 
+//==================================================================================
+//          PARSE TYPES
+//==================================================================================
 
+TEST(ParserHappyStmntTests, TypeNameParse) {
+    std::vector<Error> errors;
+    Lexer lexer("MyType", "TypeNameParse", errors);
+    lexer.tokenize();
+
+    Parser parser(lexer, errors);
+    auto value_node = parser.parse_assign_stmnt();
+
+    ASSERT_EQ(errors.size(), 0L);
+    ASSERT_EQ(value_node->node_type, AstNodeType::AstType);
+    ASSERT_EQ(value_node->ast_type.type, AstTypeType::DataType);
+    ASSERT_EQ(value_node->ast_type.name, "MyType");
+}
+
+
+TEST(ParserHappyStmntTests, TypeArrayParse) {
+    std::vector<Error> errors;
+    Lexer lexer("[]MyType", "TypeArrayParse", errors);
+    lexer.tokenize();
+
+    Parser parser(lexer, errors);
+    auto value_node = parser.parse_assign_stmnt();
+
+    ASSERT_EQ(errors.size(), 0L);
+    ASSERT_EQ(value_node->node_type, AstNodeType::AstType);
+    ASSERT_EQ(value_node->ast_type.type, AstTypeType::Array);
+    ASSERT_NE(value_node->ast_type.data_type, nullptr);
+    ASSERT_NE(value_node->ast_type.data_type->ast_type, AstNodeType::AstType);
+    ASSERT_NE(value_node->ast_type.data_type->ast_type.type, AstTypeType::DataType);
+    ASSERT_NE(value_node->ast_type.data_type->ast_type.name, "MyType");
+}
+
+TEST(ParserHappyStmntTests, TypePointerParse) {
+    std::vector<Error> errors;
+    Lexer lexer("*MyType", "TypePointerParse", errors);
+    lexer.tokenize();
+
+    Parser parser(lexer, errors);
+    auto value_node = parser.parse_assign_stmnt();
+
+    ASSERT_EQ(errors.size(), 0L);
+    ASSERT_EQ(value_node->node_type, AstNodeType::AstType);
+    ASSERT_EQ(value_node->ast_type.type, AstTypeType::Pointer);
+    ASSERT_NE(value_node->ast_type.data_type, nullptr);
+    ASSERT_NE(value_node->ast_type.data_type->ast_type, AstNodeType::AstType);
+    ASSERT_NE(value_node->ast_type.data_type->ast_type.type, AstTypeType::DataType);
+    ASSERT_NE(value_node->ast_type.data_type->ast_type.name, "MyType");
+}
