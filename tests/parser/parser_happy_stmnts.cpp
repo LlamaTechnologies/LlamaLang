d77857f4dd3d18c607817b97bea6f5bc1116bc0a
Pablo Narvaja <pnarvaja.21@gmail.com>
2021-06-07 20:49:31 -0300

    Block tests

--- tests/parser/parser_happy_stmnts.cpp
+++ tests/parser/parser_happy_stmnts.cpp
@@ -316,3 +316,93 @@ TEST(ParserHappyStmntTests, BlockNoSpaceNearCurliesTest) {
     ASSERT_EQ(ret_node->unary_expr.op, UnaryExprType::RET);
     ASSERT_NE(ret_node->unary_expr.expr, nullptr);
 }
+
+TEST(ParserHappyStmntTests, BlockSpaceNearCurliesTest) {
+    std::vector<Error> errors;
+    Lexer lexer("{ myVar i32\nret myvar }", "BlockSpaceNearCurliesTest", errors);
+    lexer.tokenize();
+
+    Parser parser(lexer, errors);
+    AstNode* value_node = parser.parse_block();
+
+    ASSERT_EQ(errors.size(), 0L);
+    ASSERT_NE(value_node, nullptr);
+    ASSERT_EQ(value_node->node_type, AstNodeType::AstBlock);
+    ASSERT_EQ(value_node->block.statements.size(), 2);
+
+    ASSERT_EQ(value_node->block.statements.at(0)->node_type, AstNodeType::AstVarDef);
+    AstNode* var_def_node = value_node->block.statements.at(0);
+    ASSERT_EQ(var_def_node->parent, value_node);
+    ASSERT_EQ(var_def_node->var_def.name, "myVar");
+    ASSERT_NE(var_def_node->var_def.type, nullptr);
+    AstNode* type_node = var_def_node->var_def.type;
+    ASSERT_EQ(type_node->node_type, AstNodeType::AstType);
+    ASSERT_EQ(type_node->ast_type.type, AstTypeType::DataType);
+    ASSERT_EQ(type_node->ast_type.name, "i32");
+
+    ASSERT_EQ(value_node->block.statements.at(1)->node_type, AstNodeType::AstUnaryExpr);
+    AstNode* ret_node = value_node->block.statements.at(1);
+    ASSERT_EQ(ret_node->parent, value_node);
+    ASSERT_EQ(ret_node->unary_expr.op, UnaryExprType::RET);
+    ASSERT_NE(ret_node->unary_expr.expr, nullptr);
+}
+
+TEST(ParserHappyStmntTests, BlockNewlinesNearCurliesTest) {
+    std::vector<Error> errors;
+    Lexer lexer("{\n\tmyVar i32\n\tret myvar\n}", "BlockNewlinesNearCurliesTest", errors);
+    lexer.tokenize();
+
+    Parser parser(lexer, errors);
+    AstNode* value_node = parser.parse_block();
+
+    ASSERT_EQ(errors.size(), 0L);
+    ASSERT_NE(value_node, nullptr);
+    ASSERT_EQ(value_node->node_type, AstNodeType::AstBlock);
+    ASSERT_EQ(value_node->block.statements.size(), 2);
+
+    ASSERT_EQ(value_node->block.statements.at(0)->node_type, AstNodeType::AstVarDef);
+    AstNode* var_def_node = value_node->block.statements.at(0);
+    ASSERT_EQ(var_def_node->parent, value_node);
+    ASSERT_EQ(var_def_node->var_def.name, "myVar");
+    ASSERT_NE(var_def_node->var_def.type, nullptr);
+    AstNode* type_node = var_def_node->var_def.type;
+    ASSERT_EQ(type_node->node_type, AstNodeType::AstType);
+    ASSERT_EQ(type_node->ast_type.type, AstTypeType::DataType);
+    ASSERT_EQ(type_node->ast_type.name, "i32");
+
+    ASSERT_EQ(value_node->block.statements.at(1)->node_type, AstNodeType::AstUnaryExpr);
+    AstNode* ret_node = value_node->block.statements.at(1);
+    ASSERT_EQ(ret_node->parent, value_node);
+    ASSERT_EQ(ret_node->unary_expr.op, UnaryExprType::RET);
+    ASSERT_NE(ret_node->unary_expr.expr, nullptr);
+}
+
+TEST(ParserHappyStmntTests, BlockSpacesBetweenNewlinesTest) {
+    std::vector<Error> errors;
+    Lexer lexer("{ \n  \t myVar i32   \n  \t  ret myvar  \n   }", "BlockSpacesBetweenNewlinesTest", errors);
+    lexer.tokenize();
+
+    Parser parser(lexer, errors);
+    AstNode* value_node = parser.parse_block();
+
+    ASSERT_EQ(errors.size(), 0L);
+    ASSERT_NE(value_node, nullptr);
+    ASSERT_EQ(value_node->node_type, AstNodeType::AstBlock);
+    ASSERT_EQ(value_node->block.statements.size(), 2);
+
+    ASSERT_EQ(value_node->block.statements.at(0)->node_type, AstNodeType::AstVarDef);
+    AstNode* var_def_node = value_node->block.statements.at(0);
+    ASSERT_EQ(var_def_node->parent, value_node);
+    ASSERT_EQ(var_def_node->var_def.name, "myVar");
+    ASSERT_NE(var_def_node->var_def.type, nullptr);
+    AstNode* type_node = var_def_node->var_def.type;
+    ASSERT_EQ(type_node->node_type, AstNodeType::AstType);
+    ASSERT_EQ(type_node->ast_type.type, AstTypeType::DataType);
+    ASSERT_EQ(type_node->ast_type.name, "i32");
+
+    ASSERT_EQ(value_node->block.statements.at(1)->node_type, AstNodeType::AstUnaryExpr);
+    AstNode* ret_node = value_node->block.statements.at(1);
+    ASSERT_EQ(ret_node->parent, value_node);
+    ASSERT_EQ(ret_node->unary_expr.op, UnaryExprType::RET);
+    ASSERT_NE(ret_node->unary_expr.expr, nullptr);
+}
