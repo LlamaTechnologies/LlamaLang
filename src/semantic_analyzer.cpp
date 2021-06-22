ca8f3260e842cbf0b42112a28a7adb1e8068a8da
Pablo Narvaja <pnarvaja.21@gmail.com>
2021-06-22 10:55:45 -0300

    Compiles

--- src/semantic_analyzer.cpp
+++ src/semantic_analyzer.cpp
@@ -1,8 +1,22 @@
 #include "semantic_analyzer.hpp"
-#include <stdarg.h>
+#include "semantic_error_msgs.hpp"
 #include "ast_nodes.hpp"
+#include <stdarg.h>
 
 static bool is_ret_stmnt(const AstNode* stmnt);
+
+static const AstNode* get_expr_type(const AstNode* expr);
+static std::unordered_map<const AstNode*, const AstNode*> cached_expr_types;
+
+/*
+Table* Table::get_parent() {
+    return parent;
+}
+
+Table& Table::create_child(const std::string& in_name) {
+    return children_scopes.emplace_back(in_name, this);
+}
+*/
 
 void Table::add_child(Table& in_child) {
     children_scopes.push_back(in_child);
@@ -13,21 +27,42 @@ void Table::remove_last_child() {
 }
 
 void Table::add_symbol(const std::string& in_name, const SymbolType in_type) {
-    Symbol symbol(in_name, in_type);
+    Symbol symbol = Symbol(in_name, in_type);
     symbols.emplace(in_name, symbol);
 }
 
-bool SemanticAnalyzer::analizeFuncProto(const AstFuncProto& in_func_proto, AstFuncDef* in_function) {
+bool SemanticAnalyzer::analizeFuncProto(const AstFuncProto& in_func_proto, AstFuncDef* in_function)/*(const AstNode* in_proto_node)*/ {
+    /*
+    assert(in_proto_node != nullptr);
+    assert(in_proto_node->node_type == AstNodeType::AstFuncProto);
+
+    const AstFuncProto& func_proto = in_proto_node->function_proto;
+    auto ret_type = func_proto.return_type;
+
+    if (ret_type->ast_type.type_id == AstTypeId::Struct) {
+        add_semantic_error(ret_type, ERROR_STRUCTS_UNSUPORTED);
+        return false;
+    }
+
+    auto str_name = std::string(func_proto.name);
+
+    symbol_table.add_symbol(str_name, SymbolType::FUNCTION);
+
+    // symbol_table = symbol_table.create_child(std::string(func_proto.name));
+    */
+
     return true;
 }
 
 bool SemanticAnalyzer::analizeFuncBlock(const AstBlock& in_func_block, AstFuncDef& in_function) {
     auto ret_type = in_function.proto->function_proto.return_type;
+
     size_t errors_before = errors.size();
 
     for (auto stmnt : in_func_block.statements) {
         if (is_ret_stmnt(stmnt)) {
-            // TODO: check if type is of type ret_type
+            auto expr_type = get_expr_type(stmnt->unary_expr.expr);
+            //check_type(expr_type, ret_type);
         }
         if (stmnt->node_type == AstNodeType::AstVarDef) {
             analizeVarDef(stmnt, false);
@@ -40,18 +75,32 @@ bool SemanticAnalyzer::analizeFuncBlock(const AstBlock& in_func_block, AstFuncDe
 }
 
 bool SemanticAnalyzer::analizeVarDef(const AstNode* in_node, const bool is_global) {
+    assert(in_node != nullptr);
+    assert(in_node->node_type == AstNodeType::AstVarDef);
+
     const AstVarDef& var_def = in_node->var_def;
-    /*
-    if (is_global && !var_def.initializer) {
-        add_semantic_error(in_node, ERROR_GLOBAL_NEED_INITIALIZER, var_def.name);
-        return false;
+    auto var_name = std::string(var_def.name);
+
+    if (is_global) {
+        if (!var_def.initializer) {
+            add_semantic_error(in_node, ERROR_GLOBAL_NEED_INITIALIZER, var_def.name);
+            return false;
+        }
+
+        //global_symbol_table.add_symbol(var_name, SymbolType::VARIABLE, in_node);
+
+        return true;
     }
 
-    if (in_var_def.initializer) {
+
+    /* Local variable */
+
+    if (var_def.initializer) {
         // TODO: check assign types
         return false;
     }
-    */
+    
+    symbol_table.add_symbol(var_name, SymbolType::VARIABLE);
 
     return true;
 }
@@ -59,6 +108,50 @@ bool SemanticAnalyzer::analizeVarDef(const AstNode* in_node, const bool is_globa
 bool SemanticAnalyzer::analizeExpr(const AstNode* in_expr) {
     return false;
 }
+
+/*
+void SemanticAnalyzer::check_type(const AstNode* type_node0, const AstNode* type_node1) {
+    assert(type_node0 != nullptr);
+    assert(type_node1 != nullptr);
+    assert(type_node0->node_type == AstNodeType::AstType);
+    assert(type_node1->node_type == AstNodeType::AstType);
+
+    const AstType& type0 = type_node0->ast_type;
+    const AstType& type1 = type_node1->ast_type;
+
+    if (type1.type_id == type0.type_id) {
+        if (type1.type_id == AstTypeId::Void || type1.type_id == AstTypeId::Bool) {
+            return;
+        }
+
+        if (type1.type_id == AstTypeId::Pointer || type1.type_id == AstTypeId::Array) {
+            check_type(type0.child_type, type1.child_type);
+            return;
+        }
+        else {
+            switch (type1.type_id) {
+            case AstTypeId::Integer:
+                if (type1.type_info->bit_size != type0.type_info->bit_size) {
+                    add_semantic_error(type_node0, ERROR_TYPES_SIZE_MISMATCH);
+                }
+                if (type1.type_info->is_signed != type0.type_info->is_signed) {
+                    add_semantic_error(type_node0, ERROR_INTEGERS_SIGN_MISMATCH);
+                }
+                return;
+            case AstTypeId::FloatingPoint:
+                if (type1.type_info->bit_size != type0.type_info->bit_size) {
+                    add_semantic_error(type_node0, ERROR_TYPES_SIZE_MISMATCH);
+                }
+                return;
+            default:
+                UNREACHEABLE;
+            }
+        }
+    }
+
+    add_semantic_error(type_node0, ERROR_TYPES_MISMATCH);
+}
+*/
 
 void SemanticAnalyzer::add_semantic_error(const AstNode* in_node, const char* in_msg, ...) {
     va_list ap, ap2;
@@ -85,3 +178,12 @@ void SemanticAnalyzer::add_semantic_error(const AstNode* in_node, const char* in
 bool is_ret_stmnt(const AstNode* stmnt) {
     return stmnt->node_type == AstNodeType::AstUnaryExpr && stmnt->unary_expr.op == UnaryExprType::RET;
 }
+
+const AstNode* get_expr_type(const AstNode* expr) {
+    if (cached_expr_types.find(expr) != cached_expr_types.end()) {
+        return cached_expr_types.at(expr);
+    }
+    // TODO(pablo96): get expr type
+    return nullptr;
+}
+
---
+++ src/semantic_error_msgs.hpp
@@ -0,0 +1,6 @@
+#pragma once
+static const char* ERROR_STRUCTS_UNSUPORTED     = "structs are not supported yet!";
+static const char* ERROR_TYPES_SIZE_MISMATCH    = "types size mismatch";
+static const char* ERROR_INTEGERS_SIGN_MISMATCH = "integers sign mismatch";
+static const char* ERROR_TYPES_MISMATCH         = "types mismatch";
+static const char* ERROR_GLOBAL_NEED_INITIALIZER= "global variable has to be initialized";
